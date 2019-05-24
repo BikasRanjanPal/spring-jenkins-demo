daf83e567d7b2051580a9452f5d70d820282e7ed
Bikas01 <palbikasranjan@gmail.com>
2019-05-24 21:24:45 +0530

    First Commit

---
+++ src/test/java/com/demo/jenkins/api/SpringJenkinsDemoApplicationTests.java
@@ -0,0 +1,24 @@
+package com.demo.jenkins.api;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJenkinsDemoApplicationTests {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsDemoApplicationTests.class);
+
+	@Test
+	public void contextLoads() {
+		logger.info("Test Case Executing....");
+		assertEquals(true, true);
+	}
+
+}
